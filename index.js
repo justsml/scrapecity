cf9e765e7cb008bc7746959a13fe099e89eca4b5
Dan Levy <397632+justsml@users.noreply.github.com>
2019-03-15 03:28:55 -0600

    Refactoring function logic
    Added named function: `getFollowerCounts`

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ import {
 } from './lib/scraper';
 
 async function go() {
-  const followerCounts = [
-    getHTML('https://instagram.com/wesbos').then(getInstagramFollowers),
-    getHTML('https://twitter.com/wesbos').then(getTwitterFollowers),
-  ];
-  
-  const [instagramCount, twCount] = await Promise.all(followerCounts);
-
+  const [instagramCount, twCount] = await getFollowerCounts('justsml')  
   console.log(
     `You have ${twCount} twitter followers and ${instagramCount} instagram followers`
   );
+}
+
+async function getFollowerCounts(username = 'wesbos') {
+  return Promise.all([
+    getHTML(`https://instagram.com/${username}`).then(getInstagramFollowers),
+    getHTML(`https://twitter.com/${username}`).then(getTwitterFollowers),
+  ]);
 }
 
 go();
