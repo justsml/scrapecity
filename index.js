5c502b6abadbc072e13ec06718e56757bbcd81cc
Dan Levy <397632+justsml@users.noreply.github.com>
2019-03-15 03:54:30 -0600

    WIP: Advanced refactor to extract general API
    More refactor examples...

--- index.js
+++ index.js
@@ -12,11 +12,37 @@ async function go() {
   );
 }
 
-async function getFollowerCounts(username = 'wesbos') {
-  return Promise.all([
-    getHTML(`https://instagram.com/${username}`).then(getInstagramFollowers),
-    getHTML(`https://twitter.com/${username}`).then(getTwitterFollowers),
-  ]);
+go();
+
+
+// Suggested other files in progress & annoted below:
+
+// ./lib/social-media-stats/index.js
+const parserServices = {
+  instagram: getInstagramFollowers,
+  twitter: getTwitterFollowers,
+  // put new social networks here!
 }
 
-go();
+const getValue = (obj, key) => typeof obj === 'object' && obj[key] ? obj[key] : '';
+
+/**
+2 formats supported:
+
+@examples
+
+  getFollowerCount('instagram', 'wesbos')
+  getFollowerCount({instagram: 'wesbos'})
+  getFollowerCount({twitter: 'wesbos'})
+
+@returns {number} followerCount
+*/
+function getFollowerCount(service, username = getValue(service, 'username')) {
+  if (!service) throw new Error(`Service (and User) arguments missing`);
+  if (!username) throw new Error(`Username argument is missing`);
+  if (!parserServices.service) throw new Error(`Service '${service}' not supported`);
+  
+  return getHTML(`https://${service}.com/${username}`)
+    .then(parserServices[service]);
+}
+
